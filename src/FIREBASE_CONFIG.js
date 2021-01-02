1511061f17c0bb42f629b9b7b14c4593cdc22890
RedCode <dannegru40@gmail.com>
2021-01-03 00:53:39 +0200

    Github Workflow

---
+++ src/FIREBASE_CONFIG.js
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyAZTDASFhFm4A6XRNnaF5hSq-eh4-icJ18",
+    authDomain: "gsm-platform-1603550979444.firebaseapp.com",
+    projectId: "gsm-platform-1603550979444",
+    storageBucket: "gsm-platform-1603550979444.appspot.com",
+    messagingSenderId: "402375878507",
+    appId: "1:402375878507:web:d446e8d5425a58a707ce2e"
+  };
